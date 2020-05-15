2670ae48e326691b5465bf8edb58cc8fc41e30cf
Grzegorz <grzegorzochodek@gmail.com>
2020-05-15 15:44:55 +0200

    Zmiany z drugiego komputera

--- src/main/java/com/ochodek/swaggertest/controller/CarController.java
+++ src/main/java/com/ochodek/swaggertest/controller/CarController.java
@@ -5,13 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/car")
 public class CarController {
 
     @GetMapping
     public String helloWorld(@RequestParam(defaultValue = "World") String name) {
-        return "Hello " + name;
+
+        return "Hello " + name + "Obecnie mamy: " + LocalDateTime.now();
     }
 
 
